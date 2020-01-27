9f4c8a51a783dc9cfdd32a91f381d419bc41fe58
Jon Jackson <jon@acuras.co.uk>
2020-01-27 17:35:10 +0000

    Add Todo functionality

--- src/forms/AddTodoForm.js
+++ src/forms/AddTodoForm.js
@@ -2,10 +2,26 @@ import React, { useState } from 'react'
 
 const AddTodoForm = props => {
 
+    const initialFormState = { id: null, title: '', description: '' }
+    const [todo, setTodo] = useState(initialFormState)
+
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        console.log(event);
+        setTodo({ ...todo, [name]: value })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        if (!todo.title) return
+        props.addTodo(todo)
+        setTodo(initialFormState)
+    }
+
     return (
-        <form className="form">
-            <input placeholder="Title" className="form-control" type="text" name="title" value="" />
-            <input placeholder="Description" className="form-control" type="text" name="description" value="" />
+        <form onSubmit={handleSubmit} className="form">
+            <input placeholder="Title" className="form-control" type="text" name="title" value={todo.title} onChange={handleInputChange} />
+            <input placeholder="Description" className="form-control" type="text" name="description" value={todo.description} onChange={handleInputChange} />
             <button className="btn btn-primary mt-3 form-control">Add New Todo</button>
         </form>
     )
