2787cd7d4ee6e8955755ecd15b8c9a1012d299b1
Jon Jackson <jon@acuras.co.uk>
2020-01-27 12:15:18 +0000

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  const title = 'Demo React App';
-  const items = [
-    'Item 1',
-    'Item 2',
-    'Item 3'
-  ];
-  return (
-    <div className="App">
-      <h1>{title}</h1>
-      {
-        items.map(item => {
-          return (
-            <li>{item}</li>
-          )
-        })
-      }
-    </div>
-  );
+class App extends Component {
+  render() {
+    const title = 'Demo React App';
+    const items = [
+      'Item 1',
+      'Item 2',
+      'Item 3'
+    ];
+    return (
+      <div className="App">
+        <h1>{title}</h1>
+        {
+          items.map(item => {
+            return (
+              <li>{item}</li>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
