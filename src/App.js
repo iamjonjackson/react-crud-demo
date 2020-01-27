0d8566836cbde9e6792e1822e19e00027528608b
Jon Jackson <jon@acuras.co.uk>
2020-01-27 16:29:06 +0000

    App.js convert from Class to Function component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    const title = 'React Todo App';
-    return (
-      <div className="App">
-        <h1>{title}</h1>
-        <div className="row mt-4">
-          <div className="col-6">
-            <h3>Add Todo</h3>
-          </div>
-          <div className="col-6">
-            <h3>View Todos</h3>
-          </div>
+const App = () => {
+  return (
+    <div className="App">
+      <h1>React Todo App</h1>
+      <div className="row mt-4">
+        <div className="col-6">
+          <h3>Add Todo</h3>
+        </div>
+        <div className="col-6">
+          <h3>View Todos</h3>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
