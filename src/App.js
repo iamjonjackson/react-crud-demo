5650fbb247514081dd7a14111fb6d281edfd38ac
Jon Jackson <jon@acuras.co.uk>
2020-01-27 12:05:29 +0000

    Render a list

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const title = 'Demo React App';
+  const items = [
+    'Item 1',
+    'Item 2',
+    'Item 3'
+  ];
   return (
     <div className="App">
-      <h1>Demo React App</h1>
+      <h1>{title}</h1>
+      {
+        items.map(item => {
+          return (
+            <li>{item}</li>
+          )
+        })
+      }
     </div>
   );
 }
